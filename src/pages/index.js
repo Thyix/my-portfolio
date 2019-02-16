6871cc69a99d0e90b5d061c28973721335accbf5
William Garneau <willi_garn@hotmail.com>
2019-02-16 16:42:08 -0500

    Last commit for today

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Boilerplate de mon portfolio fait avec Gatsby</h1>
+    <p>Fait avec Gatsby, GraphQL et React</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Page 2</Link>
   </Layout>
 )
 
